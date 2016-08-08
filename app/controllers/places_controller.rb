0b2a84c1142c6ce2cfe7a6a071e1487e6e75c1ba
Marshika Szabo <marshika.szabo@gmail.com>
2016-08-08 18:13:04 +0000

    Added all validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @places = Place.all.order("created_at ASC").page(params[:page]).per(10)
   end
