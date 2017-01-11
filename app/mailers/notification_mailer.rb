88475fcd0c496e3156f70afb694009dcf9c99098
Marshika Szabo <marshika.szabo@gmail.com>
2017-01-11 21:08:37 +0000

    Fixed s3 dev env pics

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,9 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+    mail(to: "marshika.szabo@gmail.com",
+          subject: "A comment has been added to your place")
+  end
+
 end
